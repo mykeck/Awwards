e28e44e1591e32c033ac8bc6d542f2bbcb0bb120
MikeC <kiragumike@gmail.com>
2020-10-24 03:08:21 +0300

    added files

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap4'
+    'awards.apps.AwardsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
