c07131a135c257ff6ed0ace2e678eec09b3e0ee9
MikeC <kiragumike@gmail.com>
2020-10-24 05:10:11 +0300

    craeted database

--- project/settings.py
+++ project/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap4'
+    'bootstrap4',
     'awards.apps.AwardsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+         'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'awards',
+        'USER': 'moringa',
+        'PASSWORD':'Access',
     }
 }
 
@@ -108,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
