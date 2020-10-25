9543c7036643c8282ec93d5386f343cff7dc3951
MikeC <kiragumike@gmail.com>
2020-10-25 08:36:10 +0300

    added templates

--- project/urls.py
+++ project/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.conf.urls import url,include
 from registration.backends.simple.views import RegistrationView
 from django.contrib.auth import views
+from django.contrib.auth.views import LoginView,LogoutView
 
 
 urlpatterns = [
