be62834721e4fe45d52b8ad2093ba6055b971213
MikeC <kiragumike@gmail.com>
2020-10-25 14:31:18 +0300

    added templates

--- awards/urls.py
+++ awards/urls.py
@@ -4,6 +4,10 @@ from django.conf import settings
 
 urlpatterns=[
     url(r'^$',views.homepage, name= 'homepage'),
+    url(r'^search/', views.search_results, name='search_project'),
+    url(r'^profile/',views.profile, name = "profile"),
+    url(r'^post_project/',views.post_project, name = "post_project"),
+
     
 
 ]
--- awards/views.py
+++ awards/views.py
@@ -22,9 +22,6 @@ def search_results(request):
         return render(request,'search.html',{'message':message}) 
 
 def profile(request):
-    '''
-    Displays a user's profile
-    '''
     current_user = request.user
     profile = Profile.objects.get(user =current_user)
     return render(request, 'profile.html', {"profile" : profile} )           
@@ -33,5 +30,16 @@ def profile(request):
 
 @login_required(login_url="/accounts/login/") 
 def post_project(request):
-    current_user = request.user   
+    current_user = request.user 
+
+    if request.method == "POST":
+        form = PostProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.profile = request.user
+            project.save()
+        return redirect('homepage')
+    else:
+        form = PostProjectForm()
+    return render(request, 'post_project.html', {"form": form})  
 
