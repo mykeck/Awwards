31b49873f845c71036a69438f0b8f28578a5858d
MikeC <kiragumike@gmail.com>
2020-10-27 12:34:18 +0300

    added restframework

--- awards/serializer.py
+++ awards/serializer.py
@@ -1,1 +1,13 @@
-  
+from rest_framework import serializers
+from .models import Project, Profile
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('user', 'bio', 'contact', 'profile_pic')
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta :
+        model = Project
+        fields = ('title','image', 'description','link','pubdate','profile',)
--- project/settings.py
+++ project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'registration',
     'awards',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
